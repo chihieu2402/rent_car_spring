31b9c4a29173e8c2a1a906d138ed5a631b8f0392
ChiHieu <chihieu2402@gmail.com>
2024-07-30 15:54:58 +0700

    fix layout

--- src/main/java/com/poly/controller/IndexController.java
+++ src/main/java/com/poly/controller/IndexController.java
@@ -1,14 +1,29 @@
 package com.poly.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.poly.entity.Account;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 public class IndexController {
+	@Autowired
+	HttpSession ses;
 	@RequestMapping(value="/index")
-	public String index() {
+	public String index(Model model) {
+//		Account acc = (Account) ses.getAttribute("userSes");
+//		System.out.println("User: "+acc);
+//		if(acc!=null) {
+//			model.addAttribute("log", "Welcome! "+acc.getUserName());
+//		} else {
+//			model.addAttribute("log", "Login");
+//		}
 		return "/views/index";
 	}
 
--- src/main/java/com/poly/controller/LoginController.java
+++ src/main/java/com/poly/controller/LoginController.java
@@ -10,33 +10,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.poly.dao.AccountDao;
 import com.poly.entity.Account;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 	@Autowired
 	AccountDao accDao;
-
+	@Autowired
+	HttpSession ses;
+	
+	
 	@RequestMapping(value = "/index/login")
-	public String login() {
+	public String login(Model model) {
+		
+		
 		return "/views/login";
 	}
 
 	@RequestMapping(value = "/index/login", method = RequestMethod.POST)
 	public String loginP(@RequestParam("username") String ur, @RequestParam("password") String pw, Model model) {
 		if (ur.isBlank() || pw.isBlank()) {
-			model.addAttribute("mes", "Nhập đầy đủ thông tin");
+			model.addAttribute("mes", "Enter complete information");
 			return "/views/login";
 		} else {
 			try {
 				Account acc = accDao.findByUserName(ur).get(0);
 				if(acc.getPassWord().equals(pw)) {
-					model.addAttribute("mes", "Đăng nhập thành công!");
+					ses.setAttribute("userSes",acc);
+					model.addAttribute("mes", "Login Success!");
 					return "redirect:/index";
 				} else {
-					model.addAttribute("mes", "Sai mật khẩu");
+					model.addAttribute("mes", "Invalid Username or Password");
 					return "/views/login";
 				}
 			} catch (Exception e) {
-				model.addAttribute("mes", "Tài khoản không tồn tại");
+				model.addAttribute("mes", "Invalid Username or Password");
 				return "/views/login";
 			}
 
