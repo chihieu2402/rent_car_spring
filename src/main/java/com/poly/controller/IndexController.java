0a444247578d67e06cf00b67be1743552dbeb4d3
ChiHieu <chihieu2402@gmail.com>
2024-07-30 21:48:38 +0700

    build: use fragment for layout

--- src/main/java/com/poly/controller/IndexController.java
+++ src/main/java/com/poly/controller/IndexController.java
@@ -28,10 +28,67 @@ public class IndexController {
 
 	@RequestMapping(value="/index/signup", method=RequestMethod.GET)
 	public String signup() {
+		
 		return "/views/signup";
 	}
 	@RequestMapping(value="/index/about", method=RequestMethod.GET)
-	public String about() {
+	public String about(Model model) {
+		Account acc = (Account) ses.getAttribute("userSes");
+		if(acc!=null) {
+			model.addAttribute("log",0);
+		} else {
+			model.addAttribute("log",1);
+		}
 		return "/views/about";
+	}
+	@RequestMapping(value="/index/pricing", method=RequestMethod.GET)
+	public String pricing(Model model) {
+		Account acc = (Account) ses.getAttribute("userSes");
+		if(acc!=null) {
+			model.addAttribute("log",0);
+		} else {
+			model.addAttribute("log",1);
+		}
+		return "/views/pricing";
+	}
+	@RequestMapping(value="/index/contact", method=RequestMethod.GET)
+	public String contact(Model model) {
+		Account acc = (Account) ses.getAttribute("userSes");
+		if(acc!=null) {
+			model.addAttribute("log",0);
+		} else {
+			model.addAttribute("log",1);
+		}
+		return "/views/contact";
+	}
+	@RequestMapping(value="/index/services", method=RequestMethod.GET)
+	public String services(Model model) {
+		Account acc = (Account) ses.getAttribute("userSes");
+		if(acc!=null) {
+			model.addAttribute("log",0);
+		} else {
+			model.addAttribute("log",1);
+		}
+		return "/views/services";
+	}
+	@RequestMapping(value="/index/blog", method=RequestMethod.GET)
+	public String blog(Model model) {
+		Account acc = (Account) ses.getAttribute("userSes");
+		if(acc!=null) {
+			model.addAttribute("log",0);
+		} else {
+			model.addAttribute("log",1);
+		}
+		return "/views/blog";
+	}
+	@RequestMapping(value="/index/cars", method=RequestMethod.GET)
+	public String cars(Model model) {
+		Account acc = (Account) ses.getAttribute("userSes");
+		if(acc!=null) {
+			model.addAttribute("log",0);
+		} else {
+			model.addAttribute("log",1);
+		}
+		return "/views/car";
 	}
 }
