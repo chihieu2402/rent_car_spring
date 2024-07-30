12a7df68cfca4d8bfa80f7653418e88cbdc868fe
Vutran0803 <hienciui@gmail.com>
2024-07-30 16:47:02 +0700

    vu fix layout admin

--- src/main/java/com/poly/controller/AdminController.java
+++ src/main/java/com/poly/controller/AdminController.java
@@ -1,30 +1,32 @@
 package com.poly.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.poly.dao.CarDao;
 import com.poly.entity.Car;
 import com.poly.entity.Review;
 
 @Controller
 public class AdminController {
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String index() {
-        return "views/Admin";  // No leading slash needed
-    }
+	@Autowired
+	CarDao car;
     
     @GetMapping("/thongke")
     public String Thongke(Model model) {
        
         return "views/admin/Thongke"; // Main template with dynamic content
     }
-    @GetMapping("/Car")
+    @RequestMapping("/car")
     public String car(Model model) {
-       
+    	List<Car> lcar = car.findAll();
         return "views/admin/Car"; // Main template with dynamic content
     }
 	
---
+++ src/main/java/com/poly/dao/CarDao.java
@@ -0,0 +1,9 @@
+package com.poly.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.poly.entity.Car;
+
+public interface CarDao extends JpaRepository<Car, Integer>{
+
+}
