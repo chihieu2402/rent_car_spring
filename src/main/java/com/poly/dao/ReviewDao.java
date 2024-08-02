f0dc2f8ad40e6dd8868fe5aee421a4d6519a51e0
buiminhben <quanbui.brvt@gmail.com>
2024-08-02 18:18:21 +0700

    add carDetail

--- src/main/java/com/poly/dao/ReviewDao.java
+++ src/main/java/com/poly/dao/ReviewDao.java
@@ -3,7 +3,10 @@ package com.poly.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.poly.entity.Review;
+import java.util.List;
+
 
 public interface ReviewDao extends JpaRepository<Review, Integer> {
 
+	Review findByCarID(int carID);
 }
