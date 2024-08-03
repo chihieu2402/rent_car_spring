50d096fba7c5c805c4a80887cd75c5e103cd5c1f
buiminhben <quanbui.brvt@gmail.com>
2024-08-03 16:55:35 +0700

    adf

--- src/main/java/com/poly/dao/CarDao.java
+++ src/main/java/com/poly/dao/CarDao.java
@@ -1,11 +1,14 @@
 package com.poly.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.poly.entity.Car;
 
 
 public interface CarDao extends JpaRepository<Car, Integer>{
-	Car findBycarID(Integer carID);
+	
+	List<Car> findByCarID(int carID);
 
 }
